f0f866827571cf4ae8f31ebf1d4dc331ca164a81
tima <tima1986@mail.ru>
2020-01-08 14:37:00 +0200

    first commit

---
+++ homework10/2/task2.js
@@ -0,0 +1,43 @@
+let group = [
+    {
+      name: "Alexsandr",
+      lastName: "Men'shov",
+      age: 20,
+      notebook: false
+    },
+    {
+      name: "Sergey",
+      lastName: "Trofimov",
+      age: 22,
+      notebook: true
+    },
+    {
+      name: "Svetlana",
+      lastName: "Samoilova",
+      age: 21,
+      notebook: true
+    },
+    {
+      name: "Tatyana",
+      lastName: "Obdulaeva",
+      age: 19,
+      notebook: false
+    },
+    {
+      name: "Mikhail",
+      lastName: "Romanov",
+      age: 20,
+      notebook: false
+    }
+  ];
+  function addNewStudent ( name, lastName, age, notebook) {
+      group.push({name, lastName, age, notebook})
+  }
+  addNewStudent ( "Nikolay","Savel'ev", 23, false)
+  
+  function getStudentsList(arrayOfStudents) {
+    let valueOfString = arrayOfStudents
+      .map(i => Object.values(i).join(", "))
+      .forEach(i => console.log(i));
+  }
+  getStudentsList(group);
